110efa30ad118a5633e0f6912c14a6209e48e763
Vijay Bansode <bansode.vijay1996@gmail.com>
2024-03-09 14:09:34 +0530

    new

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -2,10 +2,36 @@ import styles from "../page.module.css";
 
 export default function Contact() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <h1>Contact</h1>
+    <center>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12"></div>
+          <div className="col-md-12">
+            <label className="mt-3">
+              Do you have any question? Please do not hesitate to contact us
+              directly. Our Team will come back to you within a matter of hours
+              to help you.
+            </label>
+          </div>
+          <div className="col-md-8"></div>
+          <div className="col-md-4">
+            <div className="row">
+              <div className="col-md-12">
+                <img src="./images/microsoft.svg" />
+              </div>
+              <div className="col-md-12">
+                <label className="mb-3">Kalamboli, MH 410218</label>
+              </div>
+              <div className="col-md-12">
+                <label className="mb-3">+91 99999 88888</label>
+              </div>
+              <div className="col-md-12">
+                <label className="mb-3">career_goal@gmail.com</label>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </main>
+    </center>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,22 @@ import styles from "./page.module.css";
 // add bootstrap css
 import "bootstrap/dist/css/bootstrap.css";
 export default function Home() {
-
   return (
-    <main className={styles.main}>
-      <div className={styles.description}></div>
-    </main>
+    // <main className={styles.main}>
+    //   <div className={styles.description}>
+    <div className="container">
+      <div className="row">
+        {/* <div className="col-md-6">
+          Let me know your dreams meet the destination
+        </div> */}
+        <div className="col-md-12">
+          <center>
+            <img src="./images/confused.png" />
+          </center>
+        </div>
+      </div>
+    </div>
+    //   </div>
+    // </main>
   );
 }
